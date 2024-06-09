0c74fa624ba46269b56185099b671f609a963a65
paukars <paulsoar19@gmail.com>
2024-06-09 17:59:19 -0600

    Added lazygit.nvim plugin

---
+++ .config/nvim/lua/plugins/lazygit-nvim.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        "kdheepak/lazygit.nvim",
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
+    },
+    {
+        -- nvim v0.8.0
+        require("lazy").setup({
+            {
+                "kdheepak/lazygit.nvim",
+                dependencies = {
+                    "nvim-telescope/telescope.nvim",
+                    "nvim-lua/plenary.nvim"
+                },
+                config = function()
+                    require("telescope").load_extension("lazygit")
+                end,
+            },
+        })
+    }
+}
