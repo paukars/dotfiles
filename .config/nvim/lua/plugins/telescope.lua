4bb39c383e844a1aa7fc358de3a12fbe67d3cd2a
paukars <paulsoar19@gmail.com>
2024-06-09 15:57:29 -0600

    Modified telescope setup so it shows symlink files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -15,6 +15,11 @@ return {
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup({
+                pickers = {
+                    find_files = {
+                       find_command = { "rg", "--ignore", "-L", "--hidden", "--files"},
+                    }
+                },
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
