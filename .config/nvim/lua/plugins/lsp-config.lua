6066c59c801fcefba018da2567c099f5c5cce820
paukars <paulsoar19@gmail.com>
2024-06-09 18:34:32 -0600

    Remove 2nd instance of requiring lazy and added stylua to ensure_installed in mason

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -3,7 +3,7 @@ return {
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup({
-				ensure_installed = { "rust-analyzer", "pylint", "black", "isort" },
+				ensure_installed = { "rust-analyzer", "pylint", "black", "isort", "stylua" },
 			})
 		end,
 	},
