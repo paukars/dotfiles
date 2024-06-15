3c95804090c7ff506aa7ce09da568a84811a2ca8
paukars <paulsoar19@gmail.com>
2024-06-15 15:34:56 -0600

    Commented out presence.nvim plugin, had problems with it, added a new colorscheme and added vim-options to configure the position of screen relative to the cursor on the file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,5 +15,6 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
+require "vim-options"
 require "lazy_setup"
 require "polish"
--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -9,5 +9,6 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.everforest" },
   -- import/override with your plugins folder
 }
