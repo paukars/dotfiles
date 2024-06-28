3bde5c80ce92bbee5f801712781934fe71992ae8
paukars <paulsoar19@gmail.com>
2024-06-27 20:05:06 -0600

    Added java community

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -10,6 +10,7 @@ return {
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.everforest" },
   -- import/override with your plugins folder
